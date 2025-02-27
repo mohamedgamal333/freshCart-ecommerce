124ab7c9b15046ae68fbd63e7e237502a47e85b6
mohamedgamal333 <mohamedgamal5107@gmail.com>
2025-02-27 08:39:48 +0200

    🚀 refactor: optimize routes with Lazy Loading and Standalone Components in Angular 19

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,33 +1,31 @@
 import { Routes } from '@angular/router';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { BlankLayoutComponent } from './layouts/blank-layout/blank-layout.component';
-import { LoginComponent } from './pages/login/login.component';
-import { register } from 'module';
-import { RegisterComponent } from './pages/register/register.component';
-import { HomeComponent } from './pages/home/home.component';
-import { CartComponent } from './pages/cart/cart.component';
-import { ProductsComponent } from './pages/products/products.component';
-import { BrandsComponent } from './pages/brands/brands.component';
-import { CategoriesComponent } from './pages/categories/categories.component';
-import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { NotfoundComponent } from './pages/notfound/notfound.component';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-{path: '', component: AuthLayoutComponent ,children: [
-  {path: 'login', component: LoginComponent ,title: 'Login'},
-  {path: 'register', component: RegisterComponent ,title: 'Register'},
-]},
-{path: '', component: BlankLayoutComponent ,children: [
-  {path: 'home', component: HomeComponent ,title: 'Home'},
-  {path: 'cart', component: CartComponent ,title: 'Cart'},
-  {path: 'products', component: ProductsComponent ,title: 'Products'},
-  {path: 'brands', component: BrandsComponent ,title: 'Brands'},
-  {path: 'categories', component: CategoriesComponent ,title: 'Categories'},
-  {path: 'checkout', component: CheckoutComponent ,title: 'Checkout'},
-  {path: '**', component:NotfoundComponent ,title: 'Not Found'},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 
-]},
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      { path: 'login', loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent), title: 'Login' },
+      { path: 'register', loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent), title: 'Register' }
+    ]
+  },
 
-
+  {
+    path: '',
+    component: BlankLayoutComponent,
+    children: [
+      { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent), title: 'Home' },
+      { path: 'cart', loadComponent: () => import('./pages/cart/cart.component').then(m => m.CartComponent), title: 'Cart' },
+      { path: 'products', loadComponent: () => import('./pages/products/products.component').then(m => m.ProductsComponent), title: 'Products' },
+      { path: 'brands', loadComponent: () => import('./pages/brands/brands.component').then(m => m.BrandsComponent), title: 'Brands' },
+      { path: 'categories', loadComponent: () => import('./pages/categories/categories.component').then(m => m.CategoriesComponent), title: 'Categories' },
+      { path: 'checkout', loadComponent: () => import('./pages/checkout/checkout.component').then(m => m.CheckoutComponent), title: 'Checkout' },
+      { path: '**', component: NotfoundComponent, title: 'Not Found' }
+    ]
+  }
 ];
